b2db201c65eb7eb224a0fba212f2b6c959ca2be7
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-06-13 15:58:53 -0500

    WIP: All tasks and challenges complete for both assignments, lambda-classes.js has not been tested

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -36,10 +36,13 @@ class Person {
  * previousBackground - str 
  * className - str 
  * favSubjects - []
+ * grade - int 
  * 
  * listsSubjects() ->  logs out all of the student's favorite subjects one by one
  * prAssignment(str) ->  receives a subject as an argument and logs out a phrase 
- * sprintChallenge(str) ->  receives a subject as an argument and logs out a phrase 
+ * sprintChallenge(str) ->  receives a subject as an argument and logs out a phrase
+ * graduate() -> checks if student can graduate; continues to grade students papers until the student
+ * can graduate using a while loop; eventually logs a phrase saying the student can graduate
  */
 
 class Student extends Person {
@@ -53,6 +56,7 @@ class Student extends Person {
         this.previousBackground = studentProps.previousBackground; 
         this.className = studentProps.className;
         this.favSubjects = studentProps.favSubjects; 
+        this.grade = studentProps.grade; 
     }
 
     listsSubjects() {
@@ -67,6 +71,21 @@ class Student extends Person {
         console.log(`${this.name} has begun sprint challenge on ${subject}`);
     }
 
+    graduate(instructorObj) {
+
+        // loops until score is greater than 70
+        while (this.score < 70) {
+
+            // logs a sincere apology and informs the student that their score is being updated 
+            console.log(`${this.name}, I am sorry to inform you that you cannot graduate yet. Please continue to study hard! \nYour next assignment is being graded. Please be patient.\n`);
+            
+            // uses the instructor (or PM's) correctGrade method to change the student's score
+            instructorObj.correctGrade(this); 
+        };
+
+        console.log(`${this.name}, you can graduate! Congratulations!`);  
+    }
+
 }
 
 
@@ -79,6 +98,8 @@ class Student extends Person {
  * 
  * demo(str) -> receives a string as an argument and logs out a phrase
  * grade(obj, str) -> receives an object and a string as arguments and logs out a phrase 
+ * correctGrade(obj) -> receives a student object and changes the student object's grade value; 
+ * logs out an update
  */
 
 class Instructor extends Person {
@@ -100,6 +121,31 @@ class Instructor extends Person {
 
     grade(studentObj, subject) {
         console.log(`${studentObj.name} receives a perfect score on ${subject}`); 
+    }
+
+    correctGrade(studentObj) {
+
+        // a positive between 0 and 100
+        const points = Math.floor(Math.random() * 101) 
+
+        // -1 or 0 
+        const posOrNeg =  Math.floor((Math.random() * 2) * -1); 
+
+        
+        if (posOrNeg !== 0) {   // deducted points 
+
+            // points * -1 is a negative number 
+            studentObj.grade += points * posOrNeg; 
+
+            console.log(`${this.name} subtracted ${points} points from ${studentObj.name}'s score. \n${studentObj.name}'s new grade is ${studentObj.grade}. \n`); 
+
+        } else {  // added points  
+
+            studentObj.grade += points; 
+
+            console.log(`${this.name} added ${points} points to ${studentObj.name}'s score. \n${studentObj.name}'s new grade is ${studentObj.grade}. \n`); 
+        }
+
     }
 }
 
